f9fdb29e889fa96206fedcd305926450ca375145
chris chen <wasi.chris@gmail.com>
2022-09-01 16:57:42 +0800

    .

--- app.js
+++ app.js
@@ -4,37 +4,37 @@ const app = express()
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 
-// // import routes
-// const lab = require('./routes/lab')
-// process.title = 'mockapi'
+// import routes
+const lab = require('./routes/lab')
+process.title = 'mockapi'
 
-// // CORS All Request
-// const allowCrossDomain = (req, res, next) => {
-//   // let allowedOrigins = ['http://localhost:8081', 'http://127.0.0.1:8081']
-//   // let origin = req.headers.origin
-//   // if (allowedOrigins.includes(origin)) {
-//   //   res.header('Access-Control-Allow-Origin', origin)
-//   // }
-//   res.header('Access-Control-Allow-Origin', req.headers.origin)
-//   res.header('Access-Control-Allow-Credentials', 'true')
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-XSRF-TOKEN, True-Client-IP')
-//   next()
-// }
+// CORS All Request
+const allowCrossDomain = (req, res, next) => {
+  // let allowedOrigins = ['http://localhost:8081', 'http://127.0.0.1:8081']
+  // let origin = req.headers.origin
+  // if (allowedOrigins.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin)
+  // }
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-XSRF-TOKEN, True-Client-IP')
+  next()
+}
 
-// app.use(
-//   logger(
-//     ':date[iso] => :method :url :status :res[content-length] - :response-time ms',
-//     {
-//       skip: function (req, res) {
-//         return req.method.toUpperCase() === 'OPTIONS'
-//       }
-//     }
-//   )
-// )
-// app.use(bodyParser.json({ limit: 1024 * 1024 * 5, type: 'application/json' }))
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(allowCrossDomain)
+app.use(
+  logger(
+    ':date[iso] => :method :url :status :res[content-length] - :response-time ms',
+    {
+      skip: function (req, res) {
+        return req.method.toUpperCase() === 'OPTIONS'
+      }
+    }
+  )
+)
+app.use(bodyParser.json({ limit: 1024 * 1024 * 5, type: 'application/json' }))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(allowCrossDomain)
 
 // // set welcome route
 // const router = express.Router()
